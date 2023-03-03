b6bc7e7a55ca6d43a8a4fcdc4b09038850ccfc99
Shock-OS <carsongrover8@gmail.com>
2023-03-03 14:51:55 -0700

    7.0 Garrett Prototype 4
    Small changes to parental controls screen time limit. This commit is BEFORE the new shock-view-current-user.

---
+++ usr/bin/shock-view-current-user-wayland.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import subprocess
+import re
+
+res = subprocess.run( [ "loginctl", "--no-legend", "list-sessions" ],
+  stdout=subprocess.PIPE )
+
+for line in res.stdout.decode("utf-8").split("\n"):
+  if len(line)==0: continue
+  session, uid, user, rest = re.split( r"\s+", line, maxsplit=3 )
+  info = subprocess.run( [ "loginctl", "show-session", session ],
+     stdout=subprocess.PIPE )
+  data = {}
+  for infoline in info.stdout.decode("utf-8").split("\n"):
+    if len(infoline)==0: continue
+    key, value = re.split( "=", infoline, maxsplit=1 )
+    data[key] = value
+  if data.get("Active")=="yes" and data.get("Type")=="wayland":
+    print( user )
