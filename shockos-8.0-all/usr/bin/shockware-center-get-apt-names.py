6d74b7d563524e8433bf0f8a0322189154bea82d
Shock-OS <carsongrover8@gmail.com>
2023-06-05 20:48:58 -0700

    /usr/bin/shockware-center now displays full names for apt packages. /usr/bin/shockware-center-view-package still needs to be updated to support this change, and duplicate apps (apps that are available from both apt and flatpak) will be handeled better. 	modified:   install 	new file:   shockos-8.0-all.deb 	modified:   shockos-8.0-all/DEBIAN/postinst 	modified:   shockos-8.0-all/usr/bin/shock-os-update-manager 	modified:   shockos-8.0-all/usr/bin/shock-splash-screens 	modified:   shockos-8.0-all/usr/bin/shock-themes 	modified:   shockos-8.0-all/usr/bin/shockware-center 	new file:   shockos-8.0-all/usr/bin/shockware-center-get-apt-names.py 	new file:   shockos-8.0-all/usr/bin/shockware-center-get-apt-packages-names.py 	modified:   shockos-8.0-all/usr/bin/shockware-center-view-package

---
+++ shockos-8.0-all/usr/bin/shockware-center-get-apt-names.py
@@ -0,0 +1,29 @@
+import glob
+import gzip
+import yaml
+import os
+import fnmatch
+
+# Define the path to the .gz archives containing AppStream YAML files
+archive_files = glob.glob('/var/lib/app-info/yaml/*.yml.gz')
+
+# Retrieve the current locale
+current_locale = os.environ.get('LANG', '')
+if "_" in current_locale:
+    current_locale = current_locale.split("_")[0]
+else:
+    current_locale = current_locale.split(".")[0]
+
+# Create the wildcard pattern for the current locale
+locale_pattern = current_locale + "*"
+
+# Iterate over each archive file
+for file_path in archive_files:
+    with gzip.open(file_path, 'rt') as gz_file:
+        for document in yaml.load_all(gz_file, Loader=yaml.CSafeLoader):
+            if document.get("Type") == "desktop-application":
+                name_locale = document.get("Name", {})
+                for locale_key in name_locale.keys():
+                    if fnmatch.fnmatch(locale_key, locale_pattern):
+                        localized_name = name_locale[locale_key]
+                        print(f'"{localized_name} [APT]"')
