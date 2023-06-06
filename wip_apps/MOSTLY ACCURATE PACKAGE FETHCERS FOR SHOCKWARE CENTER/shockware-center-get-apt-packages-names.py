f884bd744ea0c56a08e2ce837dbd1fd53a73a2f8
Shock-OS <carsongrover8@gmail.com>
2023-06-06 14:36:08 -0700

    /usr/bin/shockware-center recieved a splash screen that appears when the software is launched. Support for installing/removing duplicate apps when in tabbed window still needs to be implemented. 	modified:   shockos-8.0-all.deb 	modified:   shockos-8.0-all/DEBIAN/control 	modified:   shockos-8.0-all/DEBIAN/postinst 	modified:   shockos-8.0-all/usr/bin/shockware-center 	modified:   shockos-8.0-all/usr/bin/shockware-center-view-package 	new file:   shockos-8.0-all/usr/share/shockware-center/splash.png 	new file:   shockos-8.0-all/usr/share/shockware-center/splash.svg 	new file:   wip_apps/MOSTLY ACCURATE PACKAGE FETHCERS FOR SHOCKWARE CENTER/README 	renamed:    wip_apps/read_appstream.py -> wip_apps/MOSTLY ACCURATE PACKAGE FETHCERS FOR SHOCKWARE CENTER/shockware-center-get-apt-names.py 	new file:   wip_apps/MOSTLY ACCURATE PACKAGE FETHCERS FOR SHOCKWARE CENTER/shockware-center-get-apt-packages-names.py

---
+++ wip_apps/MOSTLY ACCURATE PACKAGE FETHCERS FOR SHOCKWARE CENTER/shockware-center-get-apt-packages-names.py
@@ -0,0 +1,29 @@
+import glob
+import gzip
+import yaml
+import os
+
+# Define the path to the .gz archives containing AppStream YAML files
+archive_files = glob.glob('/var/lib/app-info/yaml/*.yml.gz')
+
+# Retrieve the current locale
+current_locale = os.environ.get('LANG', '')
+if "_" in current_locale:
+    current_locale = current_locale.split("_")[0]
+else:
+    current_locale = current_locale.split(".")[0]
+
+# Iterate over each archive file
+for file_path in archive_files:
+    with gzip.open(file_path, 'rt') as gz_file:
+        for document in yaml.load_all(gz_file, Loader=yaml.CSafeLoader):
+            if document.get("Type") == "desktop-application":
+                package = document.get("Package", "")
+                name_locale = document.get("Name", {})
+                localized_name = name_locale.get(current_locale)
+                if localized_name is None:
+                    localized_name = next(iter(name_locale.values()), None)
+                if localized_name:
+                    print(f'"{localized_name} [APT]"')
+                    print(f'"{package}"')
+
