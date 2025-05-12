0e62ca4c905ace486c7552d4ad208e59c480fb1a
Carson Grover <carsongrover8@gmail.com>
2025-05-11 18:28:00 -0700

    Ensured that each Shock OS GTK app window is isolated from other windows from the same app. 	new file:   TODO 	modified:   apps-wip/shockos-package-installer-1.0-all/usr/bin/shockos-package-installer 	modified:   apps-wip/shockos-package-installer-1.0-all/usr/share/shockos/package-installer/main.ui 	modified:   apps-wip/shockos-package-installer-1.0-all/usr/share/shockos/package-installer/shockos-package-installer.cmb 	modified:   apps-wip/shockos-update-manager-1.0-all/usr/bin/shockos-update-manager 	modified:   apps-wip/shockos-waydroid-1.0-all/usr/bin/shockos-waydroid-settings 	modified:   apps-wip/shockos-waydroid-1.0-all/usr/libexec/shockos/waydroid/get-gplay-id.sh 	modified:   apps-wip/shockos-waydroid-1.0-all/usr/share/shockos/waydroid/shockos-waydroid-settings/main.ui 	modified:   apps-wip/shockos-waydroid-1.0-all/usr/share/shockos/waydroid/shockos-waydroid-settings/shockos-waydroid-settings.cmb 	modified:   apps/GNOME/shockos-gnome-layouts-1.0-all/usr/bin/shockos-gnome-layouts 	modified:   apps/MATE/shockos-mate-backgrounds-1.0-all/usr/bin/shockos-mate-backgrounds 	modified:   apps/MATE/shockos-mate-layouts-1.0-all/usr/bin/shockos-mate-layouts 	modified:   apps/shockos-fonts-1.0-all/usr/bin/shockos-fonts 	modified:   apps/shockos-rpi5-kernel-options-1.0-all/usr/bin/shockos-rpi5-kernel-options 	modified:   apps/shockos-shockver-1.0-all/usr/bin/shockver 	modified:   apps/shockos-themes-1.0-all/usr/bin/shockos-themes 	modified:   shockos-core-10-all/usr/lib/shockos/dist-info 	modified:   templates/Python + GTK 4 App

--- apps-wip/shockos-waydroid-1.0-all/usr/libexec/shockos/waydroid/get-gplay-id.sh
+++ apps-wip/shockos-waydroid-1.0-all/usr/libexec/shockos/waydroid/get-gplay-id.sh
@@ -5,7 +5,6 @@ then
     sudo mkdir -p ~/.local/share/waydroid/data/media/0/.shockos
     sudo mount --bind /usr/share/shockos/waydroid/waydroid-shell-scripts ~/.local/share/waydroid/data/media/0/.shockos
 fi
-/usr/libexec/shockos/waydroid/start-session.sh
 id="$(sudo waydroid shell /data/media/0/.shockos/get-gplay-id.sh)"
 id="${id#*|}"
 echo "$id"
