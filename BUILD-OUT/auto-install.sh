81fc01722454db126ad1a5ff12c6c3c33efccbf5
Carson Grover <carsongrover8@gmail.com>
2025-12-27 16:20:18 -0700

    Lots of changes, I can't remember all of them. Latest one is adding blur-my-shell extension installed and enabled by default in GNOME Edition. 	new file:   BUILD-OUT/auto-install.sh 	new file:   BUILD-OUT/shockos-core_10_all.deb 	new file:   BUILD-OUT/shockos-deskenv-gnome_10_all.deb 	new file:   BUILD-OUT/shockos-fonts_1.0_all.deb 	new file:   BUILD-OUT/shockos-gnome-layouts_1.0_all.deb 	new file:   BUILD-OUT/shockos-package-installer_1.0_all.deb 	new file:   BUILD-OUT/shockos-rpi5-kernel-options_1.0_all.deb 	new file:   BUILD-OUT/shockos-shockver_1.0_all.deb 	new file:   BUILD-OUT/shockos-shockware-center_1.0_all.deb 	new file:   BUILD-OUT/shockos-themes_1.0_all.deb 	new file:   BUILD-OUT/shockos-update-manager_1.0_all.deb 	modified:   TODO 	new file:   apps-wip/shockos-welcome-1.0-all/layouts.svg 	new file:   apps-wip/shockos-welcome-1.0-all/logo.png 	modified:   apps-wip/shockos-welcome-1.0-all/main.ui 	modified:   apps-wip/shockos-welcome-1.0-all/shockos-welcome.cmb 	new file:   apps-wip/shockos-welcome-1.0-all/start-here.svg 	modified:   apps/GNOME/shockos-gnome-layouts-1.0-all/usr/bin/shockos-gnome-layouts 	modified:   apps/shockos-update-manager-1.0-all/usr/bin/shockos-update-manager 	modified:   auto-install.sh 	modified:   shockos-core-10-all/usr/lib/shockos/dist-info 	modified:   shockos-deskenv-gnome-10-all/DEBIAN/control 	modified:   shockos-deskenv-gnome-10-all/etc/dconf/db/local.d/00-shockos-gnome-defaults 	deleted:    shockos-deskenv-mate-10-all/usr/share/shockos/mate/applications-generic-names/bamf-2.index 	deleted:    shockos-deskenv-mate-10-all/usr/share/shockos/mate/applications-generic-names/libreoffice-startcenter.desktop

---
+++ BUILD-OUT/auto-install.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#this script installs the Shock OS .deb packages in the current directory and prepares the system for initial setup
+
+sudo apt update
+sudo apt update
+
+if [[ "$(whoami)" != 'shockos' ]]
+then
+    echo 'This script is ONLY meant to be run on a fresh install of Raspberry Pi OS Lite to install Shock OS. DO NOT run this script elsewhere! (Hint: Use `./build.sh [gnome/mate]` to create a build of Shock OS for installation.)'
+    exit
+fi
+
+sudo apt install -y pipewire-audio || { echo "ERROR: Failed to install package pipewire-audio. Exiting..."; exit 1; }
+
+sudo apt install -y ./"shockos-core_"* || { echo "ERROR: Failed to install shockos-core package. Exiting..."; exit 1; }
+sudo apt install -y ./"shockos-deskenv-"* || { echo "ERROR: Failed to install shockos-deskenv package. Exiting..."; exit 1; }
+
+for app in *
+do
+    if [[ "$app" == *'.deb' ]]
+    then
+        sudo apt install -y ./"$app" || { echo "ERROR: Failed to install ${app}. Exiting..."; exit 1; }
+    fi
+done
+    
+sudo apt update
+sudo apt autopurge -y
+sudo apt update
+
+source /usr/lib/shockos/dist-info
+
+# [BANDAID PATCH]: GDM THEME PATCH BEGINS
+if [[ "$SHOCKOS_DESKENV" == "GNOME" ]]
+then
+    sudo /usr/libexec/shockos/gnome/bandaids/gdm-theme-patch.sh
+fi
+# [BANDAID PATCH]: GDM THEME PATCH ENDS
+
+# Initial Setup stuff
+if [[ "$SHOCKOS_DESKENV" == "GNOME" ]]
+then
+    echo "if [[ ! -f /tmp/shockos-initsetup/started.indicator ]]
+then
+    mkdir -p /tmp/shockos-initsetup
+    touch /tmp/shockos-initsetup/started.indicator
+    dbus-run-session -- bash -c 'export XDG_VTNR=\$(fgconsole) && XDG_SESSION_TYPE=wayland gnome-session'
+fi" | tee /home/shockos/.bashrc
+elif [[ "$SHOCKOS_DESKENV" == "MATE" ]]
+then
+    echo "if [[ ! -f /tmp/shockos-initsetup/started.indicator ]]
+then
+    mkdir -p /tmp/shockos-initsetup
+    touch /tmp/shockos-initsetup/started.indicator
+    dbus-run-session -- bash -c 'export XDG_VTNR=\$(fgconsole) && XDG_SESSION_TYPE=x11 startx'
+fi" | tee /home/shockos/.bashrc
+else
+    echo 'ERROR: Could not get deskenv information from /usr/lib/shockos/dist-info. Exiting...'
+    exit 1
+fi
+mkdir -p /home/shockos/.config/autostart/
+echo '[Desktop Entry]
+Type=Application
+Exec=/usr/libexec/shockos/initsetup.sh
+Hidden=false
+Name=Shock OS Initial Setup Program
+Comment=This program runs on a fresh installation of Shock OS to allow system configuration and user creation.
+X-GNOME-Autostart-Delay=0
+X-MATE-Autostart-Delay=0' | tee /home/shockos/.config/autostart/shockos-initsetup.desktop
+
--- auto-install.sh
+++ auto-install.sh
@@ -2,6 +2,9 @@
 
 #this script installs the Shock OS .deb packages in the current directory and prepares the system for initial setup
 
+sudo apt update
+sudo apt update
+
 if [[ "$(whoami)" != 'shockos' ]]
 then
     echo 'This script is ONLY meant to be run on a fresh install of Raspberry Pi OS Lite to install Shock OS. DO NOT run this script elsewhere! (Hint: Use `./build.sh [gnome/mate]` to create a build of Shock OS for installation.)'
